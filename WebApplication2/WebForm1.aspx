dfb1282909927b311e483cf9df88c1b1273f241d
Ale1003mg <Ale1003mg@hotmail.com>
2020-10-27 16:58:15 -0600

    Respaldo de  login para active directory

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 14px;
+        }
+        .auto-style2 {
+            width: 1280px;
+            height: 258px;
+        }
+        .auto-style4 {
+            width: 284px;
+        }
+        .auto-style5 {
+            width: 182px;
+        }
+        .auto-style6 {
+            margin-left: 400px;
+        }
+    </style>
+</head>
+<body style="height: 188px">
+    <br />
+    <br />
+    <h1 class="auto-style6"><label style="text-align:center">Prueba de Active Directory a C# </label></h1>
+    <br />
+    <br />
+    <table class="auto-style2">
+        <tr>
+            <td class="auto-style5"></td>
+            <td class="auto-style4">
+                   <form id="form1" runat="server">
+                        <div ">
+                            <br />
+                            <asp:Label ID="Label1"  runat="server" Text="Usuario">Usuario</asp:Label><br />
+                             <asp:Label ID="Label3" runat="server" Text="intrucciones">Solo es el nombre no con el dominio</asp:Label><br />
+                        <asp:TextBox ID="User" runat="server" Width="222px"></asp:TextBox><br />
+                             <asp:Label ID="Label2" runat="server" Text="Contraseña"></asp:Label><br />
+                        <asp:TextBox ID="PW" runat="server" Width="225px" TextMode="Password"></asp:TextBox><br />
+                        <asp:Button ID="Button1" runat="server" Text="Ingresar" OnClick="Button1_Click" Width="232px" /><br /><br />
+                            <asp:TextBox ID="permitido" runat="server" Width="228px" Enabled="False" Height="61px" TextMode="MultiLine"></asp:TextBox><br />
+
+                        </div>
+        
+                    </form>
+
+            </td>
+            <td class="auto-style1"></td>
+        </tr>
+    </table>
+
+ 
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.DirectoryServices;
+using System.Drawing;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)//login
+        {
+
+           
+            string Server = "simpe.net";// dominio del servidor
+            string ruta = "LDAP://"+Server+ "/DC=simpe,DC=net"; //Editor ADSI
+            DirectoryEntry raiz = new DirectoryEntry();
+            raiz.Path = ruta;
+            raiz.AuthenticationType = AuthenticationTypes.Secure;
+            raiz.Username= User.Text.Trim();//Usuario
+            raiz.Password = PW.Text.Trim();//Contraseña
+            string filtro = "sAMAccountName";
+            String strSeach = filtro + "=" + User.Text.Trim();//Usuario
+            DirectorySearcher dsSystemn = new DirectorySearcher(raiz,strSeach);
+            dsSystemn.SearchScope = SearchScope.Subtree;
+
+            try
+            {
+                SearchResult srSystem = dsSystemn.FindOne();//LLama el proceso del active directory
+                string puesto = srSystem.Path.ToString();
+
+                int fount = 0;
+                int end = 0;
+                string Puestos;
+                for (int i = 0; i < puesto.Length; i++)
+                {
+                   
+                        fount = puesto.IndexOf("OU=");
+                        Puestos= puesto.Substring(fount + 3);
+                         end = Puestos.IndexOf("DC=");
+                        permitido.Text = puesto.Substring(fount + 3,end-1);
+                }
+              
+
+                permitido.BackColor = Color.Green;
+                permitido.ForeColor = Color.White;
+             
+            }
+            catch (Exception prueba)
+            {
+                permitido.BackColor = Color.Red;
+                  permitido.ForeColor = Color.Black;
+                   permitido.Text = "Acceso denegado";
+
+            }
+
+
+          
+        }
+
+    }
+}
